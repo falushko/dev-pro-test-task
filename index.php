d95b0a5197d1ca9b696462541dd2f5b5e58c1982
Vladimir Falushko <vov8278@gmail.com>
2017-09-14 22:55:14 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require "vendor/autoload.php";
+
+use App\Parser;
+use MongoDB\Client as Mongo;
+
+$config = yaml_parse(file_get_contents('config.yml'));
+$parser = new Parser($config['website']);
+$words = new SplFileObject($config['words']);
+$wordsCollection = (new Mongo)->test_task->words;
+
+while (!$words->eof()) {
+	$word = $words->fgets();
+	$result = $parser->parse(trim($word));
+	$insertOneResult = $wordsCollection->insertOne($result);
+
+	echo $insertOneResult->getInsertedId() . "\n";
+}
+
+$words = null;
---
+++ src/Parser.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use GuzzleHttp\Client as HttpClient;
+use PHPHtmlParser\Dom;
+
+/**
+ * This class is used to parse search result for $word from $website.
+ * Class Parser
+ * @package App
+ */
+class Parser
+{
+	private $website;
+	private $httpOptions = [
+		'headers' => [
+			'User-Agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13',
+		]
+	];
+
+	public function __construct(string $website)
+	{
+		$this->website = $website;
+	}
+
+	/**
+	 * This function makes the things done.
+	 * @param string $word
+	 * @return array
+	 */
+	public function parse(string $word) : array
+	{
+		$client = new HttpClient(['base_uri' => $this->website]);
+		$html = (string) $client->get($word, $this->httpOptions)->getBody();
+
+		$dom = new Dom;
+		$dom->load($html);
+
+		$content = trim((string) $dom->find('.content div#article')->innerHtml());
+
+		return ['word' => $word, 'content' => $content];
+	}
+}
